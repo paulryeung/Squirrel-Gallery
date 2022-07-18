c5e98e3eadb3334a450b6bca77af7070b35a79e6
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-18 16:24:39 -0400

    updated index page

--- main_app/views.py
+++ main_app/views.py
@@ -3,16 +3,17 @@ from django.http import HttpResponse
 
 #For now put models here
 class Squirrel:
-    def __init__(self, name, species, description, country_found):
+    def __init__(self, name, species, description, country_found, age):
         self.name = name
         self.species = species
         self.description = description
         self.country_found = country_found
+        self.age = age
 
 squirrels = [
-    Squirrel('Bob', "Eastern Gray", "gray and bushy", "Canada"),
-    Squirrel('Nick', "American Red", "red and fury", "United States"),
-    Squirrel('Jumpy', "Northern Flying", "jumps and glides", "Canada")
+    Squirrel('Bob', "Eastern Gray", "gray and bushy", "Canada", 2),
+    Squirrel('Nick', "American Red", "red and fury", "United States", 0),
+    Squirrel('Jumpy', "Northern Flying", "jumps and glides", "Canada, United States", 4)
 ]
 
 
